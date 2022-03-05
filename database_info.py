06e4bee197bc92094db08f7cd187abbfe9756897
Zach Carlson <zcarlson@umich.edu>
2022-03-05 14:12:22 -0500

    Delete database_info.py

--- database_info.py
+++
@@ -1,3 +0,0 @@
-#database_info.py file is to store user specific connection string, replace the <> with your details
-
-connection_string = 'mysql+mysqldb://<username>:<password>@<localhost>/<database>'
