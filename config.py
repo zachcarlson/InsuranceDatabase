3f8d2f81b33de47dee3abda4a27d129e1cf0af60
Zach Carlson <zcarlson@umich.edu>
2022-03-05 13:58:43 -0500

    Update and rename config.py to database_info.py
    Python packages exist called config, to avoid confusion and unintentional changes, we're renaming to database_info

--- config.py
+++
@@ -1,6 +0,0 @@
-#config.py file is to store user specific connection string details, replace the '' with your details
-
-username = ''
-password = ''
-localhost = ''
-database = ''
---
+++ database_info.py
@@ -0,0 +1,3 @@
+#database_info.py file is to store user specific connection string, replace the <> with your details
+
+connection_string = 'mysql+mysqldb://<username>:<password>@<localhost>/<database>'
